e46f32d4d90bc6dbc46a38bc7b4870fa93e07b73
Darthveloper21 <hienvungoc01@gmail.com>
2022-08-25 20:31:54 +0700

    init

---
+++ script.js
@@ -0,0 +1,23 @@
+function getLttImage() {
+    fetch('data.json')
+    .then(res => res.json())
+    .then(data => {
+        let image64 = data["image64"];
+        document.body.style.backgroundImage = "url('data:image/png;base64," + image64 + "')";
+
+        let video = data["video"];
+        
+        let videoElement = document.createElement('video');
+        
+        videoElement.src = "data:video/webm;base64," + video;
+        videoElement.autoplay = false;
+        videoElement.loop = false;
+        videoElement.controls = true;
+        videoElement.muted = false;
+
+        let box = document.getElementById('box');
+        box.appendChild(videoElement);
+    });
+}
+
+getLttImage()
